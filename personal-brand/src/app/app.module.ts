be8eeb27ad52bde50fc9fbedc8a72c3aab5e869f
Fisher <UA26358@DOW.COM>
2018-07-02 14:13:00 -0400

    Added home and education components, beginning routing

--- personal-brand/src/app/app.module.ts
+++ personal-brand/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
+import { EducationComponent } from './education/education.component';
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes: Routes =[
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'education', component: EducationComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducationComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
